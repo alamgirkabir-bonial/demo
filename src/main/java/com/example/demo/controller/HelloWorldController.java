54b48affc0b2646d5b2697b44f634ef676f20f53
Alamgir Kabir <alamgir.kabir@bonial.com>
2025-04-16 21:28:40 +0200

    chore: add documentation

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -6,11 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    /**
+     * This is a simple REST controller that provides two endpoints:
+     * /hello and /goodbye.
+     *
+     * @return A greeting message.
+     */
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
 
+    /**
+     * This endpoint returns a goodbye message.
+     *
+     * @return A goodbye message.
+     */
     @GetMapping("/goodbye")
     public String goodbye() {
         return "Goodbye, World!";
